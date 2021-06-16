9e7a8671ef1013913a061c0781b83802c6ff141f
Brad Davidson <brad.davidson@rancher.com>
2021-06-16 10:20:13 -0700

    Bump job image to include helm-set-status plugin (#103)
    * Bump job image to include helm-set-status plugin
    
    Signed-off-by: Brad Davidson <brad.davidson@rancher.com>

--- pkg/helm/controller.go
+++ pkg/helm/controller.go
@@ -30,7 +30,7 @@ import (
 var (
 	trueVal         = true
 	commaRE         = regexp.MustCompile(`\\*,`)
-	DefaultJobImage = "rancher/klipper-helm:v0.6.0-build20210608"
+	DefaultJobImage = "rancher/klipper-helm:v0.6.1-build20210616"
 )
 
 type Controller struct {
