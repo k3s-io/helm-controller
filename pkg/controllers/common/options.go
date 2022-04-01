583d63d2d23f8c04b7984048082e47bda6630a95
Arvind Iyengar <arvind.iyengar@rancher.com>
2022-04-01 13:17:59 -0700

    Move controller logic to controllers package and use wrangler-cli+cobra instead of urfave/cli
    Signed-off-by: Arvind Iyengar <arvind.iyengar@rancher.com>

---
+++ pkg/controllers/common/options.go
@@ -0,0 +1,15 @@
+package common
+
+import "fmt"
+
+// Options defines options that can be set on initializing the Helm Controller
+type Options struct {
+	Threadiness int
+}
+
+func (opts Options) Validate() error {
+	if opts.Threadiness <= 0 {
+		return fmt.Errorf("cannot start with thread count of %d, please pass a proper thread count.", opts.Threadiness)
+	}
+	return nil
+}
