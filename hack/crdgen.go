e64bc277c69c33df6516199040c9e0fbc203a98b
Arvind Iyengar <arvind.iyengar@rancher.com>
2022-09-07 15:23:51 -0700

    Move CRD list logic to CRD package for other packages to consume
    Signed-off-by: Arvind Iyengar <arvind.iyengar@rancher.com>

--- hack/crdgen.go
+++ hack/crdgen.go
@@ -3,22 +3,11 @@ package main
 import (
 	"os"
 
-	v1 "github.com/k3s-io/helm-controller/pkg/apis/helm.cattle.io/v1"
+	"github.com/k3s-io/helm-controller/pkg/crd"
 	_ "github.com/k3s-io/helm-controller/pkg/generated/controllers/helm.cattle.io/v1"
-	"github.com/rancher/wrangler/pkg/crd"
+	wcrd "github.com/rancher/wrangler/pkg/crd"
 )
 
 func main() {
-	chart := crd.NamespacedType("HelmChart.helm.cattle.io/v1").
-		WithSchemaFromStruct(v1.HelmChart{}).
-		WithColumn("Job", ".status.jobName").
-		WithColumn("Chart", ".spec.chart").
-		WithColumn("TargetNamespace", ".spec.targetNamespace").
-		WithColumn("Version", ".spec.version").
-		WithColumn("Repo", ".spec.repo").
-		WithColumn("HelmVersion", ".spec.helmVersion").
-		WithColumn("Bootstrap", ".spec.bootstrap")
-	config := crd.NamespacedType("HelmChartConfig.helm.cattle.io/v1").
-		WithSchemaFromStruct(v1.HelmChartConfig{})
-	crd.Print(os.Stdout, []crd.CRD{chart, config})
+	wcrd.Print(os.Stdout, crd.List())
 }
