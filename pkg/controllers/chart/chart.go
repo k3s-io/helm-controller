63d267c9f29c690529ec9cfae885e006a0bc5694
Guilherme Macedo <guilherme@gmacedo.com>
2023-01-19 17:01:15 -0300

    Major versions bumps in Go, Alpine and Dapper (#169)
    * Major versions bumps in Go, Alpine and Dapper
    * Downgrade Dapper ARM version
    * Bump klipper-helm image
    * plugins/<image>:latest is no longer published for ARM 32-bit
    
    Signed-off-by: Guilherme Macedo <guilherme.macedo@suse.com>

--- pkg/controllers/chart/chart.go
+++ pkg/controllers/chart/chart.go
@@ -53,7 +53,7 @@ const (
 var (
 	commaRE              = regexp.MustCompile(`\\*,`)
 	deletePolicy         = metav1.DeletePropagationForeground
-	DefaultJobImage      = "rancher/klipper-helm:v0.7.4-build20221121"
+	DefaultJobImage      = "rancher/klipper-helm:v0.7.5-build20230118"
 	DefaultFailurePolicy = FailurePolicyReinstall
 )
 
