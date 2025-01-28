5d420888b5670990c69379caed5682382c92b4a0
Derek Nola <derek.nola@suse.com>
2025-01-28 10:11:35 -0800

    Remove wrangler-cli dependency, use regular cobra cli
    Signed-off-by: Derek Nola <derek.nola@suse.com>

--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/onsi/ginkgo/v2 v2.22.2
 	github.com/onsi/gomega v1.36.2
 	github.com/rancher/lasso v0.0.0-20250109193533-00757eec2dbd
-	github.com/rancher/wrangler-cli v0.0.0-20220624114648-479c5692ba22
 	github.com/rancher/wrangler/v3 v3.1.0
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/cobra v1.8.1
@@ -55,7 +54,6 @@ require (
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
-	github.com/rancher/wrangler v0.8.8 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	golang.org/x/mod v0.22.0 // indirect
@@ -76,7 +74,6 @@ require (
 	k8s.io/code-generator v0.31.1 // indirect
 	k8s.io/gengo v0.0.0-20240826214909-a7b603a56eb7 // indirect
 	k8s.io/gengo/v2 v2.0.0-20240228010128-51d4e06bde70 // indirect
-	k8s.io/klog v1.0.0 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
---
+++ pkg/cli/cli.go
@@ -0,0 +1,120 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"k8s.io/klog/v2"
+
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/k3s-io/helm-controller/pkg/controllers"
+	"github.com/k3s-io/helm-controller/pkg/controllers/common"
+	"github.com/k3s-io/helm-controller/pkg/crd"
+	wcrd "github.com/rancher/wrangler/v3/pkg/crd"
+	_ "github.com/rancher/wrangler/v3/pkg/generated/controllers/apiextensions.k8s.io"
+	_ "github.com/rancher/wrangler/v3/pkg/generated/controllers/networking.k8s.io"
+	"github.com/rancher/wrangler/v3/pkg/kubeconfig"
+	"github.com/rancher/wrangler/v3/pkg/ratelimit"
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/tools/clientcmd"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+type HelmController struct {
+	Debug           bool
+	DebugLevel      int
+	Kubeconfig      string
+	MasterURL       string
+	Namespace       string
+	Threads         int
+	ControllerName  string
+	NodeName        string
+	JobClusterRole  string
+	DefaultJobImage string
+	PprofPort       int
+}
+
+func (hc *HelmController) MustSetupDebug() {
+	err := hc.SetupDebug()
+	if err != nil {
+		panic("failed to setup debug logging: " + err.Error())
+	}
+}
+
+func (hc *HelmController) SetupDebug() error {
+	logging := flag.NewFlagSet("", flag.PanicOnError)
+	klog.InitFlags(logging)
+	if hc.Debug {
+		logrus.SetLevel(logrus.DebugLevel)
+		if err := logging.Parse([]string{
+			fmt.Sprintf("-v=%d", hc.DebugLevel),
+		}); err != nil {
+			return err
+		}
+	} else {
+		if err := logging.Parse([]string{
+			"-v=0",
+		}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (a *HelmController) Run(cmd *cobra.Command, args []string) error {
+	if a.Debug && a.PprofPort > 0 {
+		go func() {
+			// Serves HTTP server runtime profiling data in the format expected by the
+			// pprof visualization tool at the provided endpoint on the local network
+			// See https://pkg.go.dev/net/http/pprof?utm_source=gopls for more information
+			log.Println(http.ListenAndServe(fmt.Sprintf("localhost:%d", a.PprofPort), nil))
+		}()
+	}
+	a.MustSetupDebug()
+
+	cfg := a.GetNonInteractiveClientConfig()
+
+	clientConfig, err := cfg.ClientConfig()
+	if err != nil {
+		return err
+	}
+	clientConfig.RateLimiter = ratelimit.None
+
+	ctx := cmd.Context()
+	if err := wcrd.Create(ctx, clientConfig, crd.List()); err != nil {
+		return err
+	}
+
+	opts := common.Options{
+		Threadiness:     a.Threads,
+		NodeName:        a.NodeName,
+		JobClusterRole:  a.JobClusterRole,
+		DefaultJobImage: a.DefaultJobImage,
+	}
+
+	if err := opts.Validate(); err != nil {
+		return err
+	}
+
+	if err := controllers.Register(ctx, a.Namespace, a.ControllerName, cfg, opts); err != nil {
+		return err
+	}
+
+	<-cmd.Context().Done()
+	return nil
+}
+
+func (a *HelmController) GetNonInteractiveClientConfig() clientcmd.ClientConfig {
+	// Modified https://github.com/rancher/wrangler/blob/3ecd23dfea3bb4c76cbe8e06fb158eed6ae3dd31/pkg/kubeconfig/loader.go#L12-L32
+	return clientcmd.NewInteractiveDeferredLoadingClientConfig(
+		kubeconfig.GetLoadingRules(a.Kubeconfig),
+		&clientcmd.ConfigOverrides{
+			ClusterDefaults: clientcmd.ClusterDefaults,
+			ClusterInfo:     clientcmdapi.Cluster{Server: a.MasterURL},
+		}, nil)
+}
